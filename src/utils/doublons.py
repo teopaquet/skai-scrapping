1cc7a16a0a578931e935fc67e4b8b92de8e474be
HenryFalko <henrydelongevialle@gmail.com>
2025-07-21 13:34:15 +0200

    Refactor doublons.py to update CSV path and add duplicate detection
    - Changed the CSV file path to 'airlines_name_clean.csv'.
    - Added functionality to identify and print duplicate rows before removing them.
    - Updated comments for clarity on the purpose of the code.

--- src/utils/doublons.py
+++ src/utils/doublons.py
@@ -3,14 +3,23 @@ import os
 
 # Chemin du fichier à nettoyer (toujours relatif au script)
 base_dir = os.path.dirname(os.path.abspath(__file__))
-csv_path = os.path.join(base_dir, '../../data/raw/linkedin_list/linkedin_list_merged_with_fleet.csv')
+csv_path = os.path.join(base_dir, '../../data/raw/airlines_name_clean.csv')
 csv_path = os.path.abspath(csv_path)
 print(csv_path)
 
 # Charger le CSV
 df = pd.read_csv(csv_path)
 
-# Supprimer les doublons stricts (toutes colonnes identiques)
+
+# Trouver les doublons stricts (toutes colonnes identiques)
+duplicates = df[df.duplicated(keep=False)]
+if not duplicates.empty:
+    print("Lignes supprimées :")
+    print(duplicates)
+else:
+    print("Aucune ligne supprimée.")
+
+# Supprimer les doublons stricts
 df_clean = df.drop_duplicates()
 
 # Réécrire le fichier (même nom, écrasement)
